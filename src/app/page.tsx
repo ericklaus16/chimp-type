e944a97eabac801a1f81a0c67aa6fbe2054616c0
Eric Klaus <ericklaus16@gmail.com>
2024-01-02 23:44:53 -0300

    Arquivos iniciais com jogo semi-funcional

---
+++ src/app/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+import { useEffect, useState } from 'react';
+import '../styles/main.css';
+
+import { LoremIpsum } from 'lorem-ipsum';
+
+export default function Home() {
+  const [wpm, setWPM] = useState<number>(0);
+  const [charactersTyped, setCharactersTyped] = useState<number>(0);
+  const [timerPlaceholder, setTimePlaceholder] = useState<number>(15);
+  const [isTimerRunning, setTimeRunning] = useState<boolean>(false);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+  const [activeTimer, setActiveTimer] = useState(15);
+  const [textToBeTyped, setTextToBeTyped] = useState("");
+  const [textTyped, setTextTyped] = useState("");
+  const lorem = new LoremIpsum();
+
+  useEffect(() => {
+    handleGenerateText();
+    setTimePlaceholder(activeTimer);
+  }, []);
+
+  useEffect(() => {
+    let timerInterval: NodeJS.Timeout;
+
+    if (isTimerRunning) {
+      timerInterval = setInterval(() => {
+        setTimePlaceholder((prevTime) => {
+          const newTime = prevTime - 1;
+
+          if (newTime === 0) {
+            clearInterval(timerInterval);
+            setGameOver(true);
+          }
+
+          return newTime;
+        });
+        
+      }, 1000);
+    }
+
+    return () => clearInterval(timerInterval);
+  }, [isTimerRunning]);
+
+  useEffect(() => {
+    if(gameOver){
+      setCharactersTyped(textTyped.length);
+      
+      let count = 0;
+
+      for (let i = 0; i < textTyped.length; i++) {
+        if (textTyped[i] === " ") {
+          count++;
+        }
+      }
+      
+      setWPM((count + 1) * 4);
+    }
+  }, [gameOver]);
+
+  const handleTimerClick = (timer: number) => {
+    setActiveTimer(timer);
+    setTimePlaceholder(timer);
+    handleGenerateText();
+  };
+
+  const handleGenerateText = () => {
+    setTextToBeTyped(lorem.generateSentences(2 * (activeTimer / 15))); 
+  }; 
+
+  return (
+    <div className="content">
+      <div className='header'>
+        <img src='chimp.jpg' className='chimpLogo'/>
+        <h1>CHIMP-TYPE</h1>
+      </div>
+      {!gameOver && (
+        <div className="game">
+          {!isTimerRunning && (
+          <div className="timers">
+            <img src={"timer.png"} className="timerLogo"/>
+            <div className="timings">
+              <button onClick={() => handleTimerClick(15)} className={activeTimer === 15 ? "activeTimer" : "defaultTimer"}>15s</button>
+              <button onClick={() => handleTimerClick(30)} className={activeTimer === 30 ? "activeTimer" : "defaultTimer"}>30s</button>
+              <button onClick={() => handleTimerClick(60)} className={activeTimer === 60 ? "activeTimer" : "defaultTimer"}>60s</button>
+            </div>
+          </div>
+          )}
+          {isTimerRunning && (<p style={{color: "white", fontSize: 30}}>{timerPlaceholder}</p>)}
+          <div className="typing">
+            <div className="typingZone">
+              <div style={{ position: 'relative', width: '90vw' }}>
+                <p className='guideText'>{textToBeTyped || 'Texto de fundo'}</p>
+                <textarea 
+                className="gameInput" 
+                onChange={
+                  (text) => {
+                    setTextTyped(text.target.value); 
+                    if(!isTimerRunning) {
+                      setTimeRunning(true);
+                    }
+                  }
+                } 
+                autoFocus/>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {gameOver && (
+        <div>
+          <div className='wpmCounter'>
+            <p>wpm:</p>
+            <p>{wpm}</p>
+          </div>
+          <div className='accuracy'>
+            <p>acc:</p>
+            <p>100%</p>
+          </div>
+          <button 
+            className="playAgainButton" 
+            onClick={() => {
+              setGameOver(false); 
+              setTimePlaceholder(activeTimer); 
+              setTimeRunning(false); 
+              handleGenerateText();}}
+            >
+              Play Again!
+            </button>
+        </div>
+      )}
+    </div>
+  );
+}
